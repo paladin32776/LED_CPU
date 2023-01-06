2bed10b99ec6b6f449e84702d99857e2a1158b75
gfattinger <gernot.fattinger@ilutions.net>
2023-01-06 08:06:06 -0500

    Release v1.0
    Works fully for both ALU and MANUAl boards.
    Coded for PCA9955, needs modifications for PCA9955B.
    
    To do for next version: auto detect PCA9955 vs PCA9955B ...

--- LED_CPU.cpp
+++ LED_CPU.cpp
@@ -2,19 +2,19 @@
 
 LED_ALU::LED_ALU()
 {
-	leds1 = new PCA9955B(01);
-	leds2 = new PCA9955B(05);
-	leds1->mode(PCA9955B::PWM0);
+	leds1 = new PCA9955(PCA_1_ADDRESS);
+	leds2 = new PCA9955(PCA_2_ADDRESS);
+	leds1->mode(PCA9955::PWM0);
 	leds1->dutycycle(PWM_GREEN);
 	leds1->dutycycle(12,PWM_RED); // eADDSUB
 	leds1->dutycycle(13,PWM_RED); // eRA
 	leds1->dutycycle(14,PWM_RED); // eRB
 	leds1->dutycycle(15,PWM_RED); // eG1
-	leds2->mode(PCA9955B::PWM0);
+	leds2->mode(PCA9955::PWM0);
 	leds2->dutycycle(PWM_GREEN);
 	leds2->dutycycle(4,PWM_RED);  // eRC
-	leds2->dutycycle(5,PWM_BLUE); // NEG
-	leds2->dutycycle(6,PWM_BLUE); // OVERFLOW
+	leds2->dutycycle(5,PWM_BLUE); // neg
+	leds2->dutycycle(6,PWM_BLUE); // overflow
 	leds2->dutycycle(7,PWM_RED);   // eG2
 }
 
@@ -26,28 +26,28 @@ unsigned char LED_ALU::reverse4bit(unsigned char x)
 	return xout;
 }
 
-void LED_ALU::update(unsigned char BUS, unsigned char RA, unsigned char RB, 
-				 		  unsigned char RC, unsigned char CTRL, unsigned char NEG, 
-				 		  unsigned char OVERFLOW)
+void LED_ALU::update(unsigned char bus, unsigned char ra, unsigned char rb, 
+				 		  unsigned char rc, unsigned char ctrl, unsigned char neg, 
+				 		  unsigned char overflow)
 {
-	unsigned short leddata1 = (BUS & 0x0F) + ((RA & 0x0F)<<4) 
-	                        + ((reverse4bit(RB) & 0x0F)<<8) 
-	                        + (bitRead(CTRL, CTRL_BIT_PM)<<12)
-	                        + (bitRead(CTRL, CTRL_BIT_RA)<<13)
-	                        + (bitRead(CTRL, CTRL_BIT_RB)<<14)
-	                        + (bitRead(CTRL, CTRL_BIT_G1)<<15);
-	unsigned short leddata2 = (reverse4bit(RC) & 0x0F)
-	                        + (bitRead(CTRL, CTRL_BIT_RC)<<4)
-	                        + ((NEG & 1)<<5) + ((OVERFLOW & 1)<<6)
-	                        + (bitRead(CTRL, CTRL_BIT_G2)<<7);
+	unsigned short leddata1 = (bus & 0x0F) + ((ra & 0x0F)<<4) 
+	                        + ((reverse4bit(rb) & 0x0F)<<8) 
+	                        + (bitRead(ctrl, CTRL_BIT_PM)<<12)
+	                        + (bitRead(ctrl, CTRL_BIT_RA)<<13)
+	                        + (bitRead(ctrl, CTRL_BIT_RB)<<14)
+	                        + (bitRead(ctrl, CTRL_BIT_G1)<<15);
+	unsigned short leddata2 = (reverse4bit(rc) & 0x0F)
+	                        + (bitRead(ctrl, CTRL_BIT_RC)<<4)
+	                        + ((neg & 1)<<5) + ((overflow & 1)<<6)
+	                        + (bitRead(ctrl, CTRL_BIT_G2)<<7);
 	leds1->set_leds(leddata1);
 	leds2->set_leds(leddata2);
 }
 
 LED_MANUAL::LED_MANUAL()
 {
-	leds1 = new PCA9955B(01);
-	leds1->mode(PCA9955B::PWM0);
+	leds1 = new PCA9955(PCA_1_ADDRESS);
+	leds1->mode(PCA9955::PWM0);
 	leds1->dutycycle(PWM_GREEN);
 	leds1->dutycycle(10,PWM_RED); // eADDSUB
 	leds1->dutycycle(11,PWM_RED); // eRA
@@ -65,9 +65,9 @@ unsigned char LED_MANUAL::reverse4bit(unsigned char x)
 	return xout;
 }
 
-void LED_MANUAL::update(unsigned char DATA, unsigned char CTRL)
+void LED_MANUAL::update(unsigned char data, unsigned char ctrl)
 {
-	unsigned short leddata1 = (DATA & 0x0F) + ((CTRL & 0x3F)<<4);
+	unsigned short leddata1 = (data & 0x0F) + ((ctrl & 0x3F)<<4);
 	leds1->set_leds(leddata1);
 }
 
--- LED_CPU.h
+++ LED_CPU.h
@@ -1,5 +1,11 @@
 #include "Arduino.h"
-#include "PCA9955B.h"
+#include "PCA9955.h"
+
+// #define PCA_1_ADDRESS 0x01
+// #define PCA_2_ADDRESS 0x05
+
+#define PCA_1_ADDRESS 0x60
+#define PCA_2_ADDRESS 0x61
 
 #define PWM_GREEN 6
 #define PWM_RED 6
@@ -14,38 +20,38 @@
 
 // LED connections on ALU board:
 // =============================
-// led1_0..led1_3 : BUS 0..3
-// led1_4..led1_7 : RA 0..3
-// led1_8..led1_11 : RB 3..0
+// led1_0..led1_3 : bus 0..3
+// led1_4..led1_7 : ra 0..3
+// led1_8..led1_11 : rb 3..0
 // led1_12 : ePM
 // led1_13 : eRA
 // led1_14 : eRB
 // led1_15 : eG1
 
-// led2_0..led2_3 : RC 3..0
+// led2_0..led2_3 : rc 3..0
 // led2_4 : eRC
-// led2_5 : NEG
-// led2_6 : OVERFLOW
+// led2_5 : neg
+// led2_6 : overflow
 // led2_7 : eG2
 
 class LED_ALU
 {
 	private:
-		PCA9955B *leds1, *leds2;
+		PCA9955 *leds1, *leds2;
 		unsigned char reverse4bit(unsigned char x);
 	public:
 		LED_ALU();
-		void update(unsigned char BUS, unsigned char RA, unsigned char RB, 
-  						 unsigned char RC, unsigned char CTRL, unsigned char NEG, 
-  						 unsigned char OVERFLOW);
+		void update(unsigned char bus, unsigned char ra, unsigned char rb, 
+  						 unsigned char rc, unsigned char ctrl, unsigned char neg, 
+  						 unsigned char overflow);
 };
 
 class LED_MANUAL
 {
 	private:
-		PCA9955B *leds1;
+		PCA9955 *leds1;
 		unsigned char reverse4bit(unsigned char x);
 	public:
 		LED_MANUAL();
-		void update(unsigned char DATA, unsigned char CTRL);
+		void update(unsigned char data, unsigned char ctrl);
 };
